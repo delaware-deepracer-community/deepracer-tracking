5342ae994f07ac3122ed54076759ad069feab435
EC2 Default User <ec2-user@ip-172-31-29-150.ec2.internal>
2023-04-25 10:40:15 +0000

    Added cloud watch loggin class

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 # import logging
 # from deepracer import boto3_enhancer
 import os
+import boto3
 
 DURATION_THRESHOLD = 1
 
@@ -9,3 +10,5 @@ GOOGLE_DDNS_UNAME = os.getenv('GOOGLE_DDNS_UNAME')
 GOOGLE_DDNS_PWD = os.getenv('GOOGLE_DDNS_PWD')
 SG_ID = os.getenv("SG_ID")
 PL_ID = os.getenv("PL_ID")
+
+LOG_CLIENT = boto3.client('logs', region_name='us-east-1')
---
+++ cwatch_logging.py
@@ -0,0 +1,34 @@
+from config import settings
+from datetime import datetime
+
+class CWatch_logging:
+    """
+    This class for logging messaged to CloudWatch log stream
+    """
+    
+    def __init__(self, logGroupName=None, logStreamName=None):
+        """
+        This is a constructor to initialize with log group and logstream
+        """
+        self.logGroupName = logGroupName if logGroupName is not None else 'deepracer-tracking-log-group'
+        self.logStreamName = logStreamName if logStreamName is not None else 'deepracer-tracking-log-stream'
+        self.cwLogClient = settings.LOG_CLIENT
+        
+    def send_log(self, message):
+        """
+        This is a method to put log events on a log stream
+        """
+        response = self.cwLogClient.put_log_events(
+            logGroupName=self.logGroupName,
+            logStreamName=self.logStreamName,
+            logEvents=[
+                {
+                    'timestamp': int(round(datetime.now().timestamp()*1000, 0)),
+                    'message': message
+                },
+            ]
+        )
+        
+testLog = CWatch_logging()
+for i in range(100):
+    testLog.send_log(f'This is test message from python class {i}')
